e6b26d3d2889690af8ed06a07f4a139e36fba8a9
M513-T\admin <elaine@ncut.edu.tw>
2020-10-21 11:13:25 +0800

    練習 1之1 設定 Route 回傳字串

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
     return'welcome';
 });
-Route::get('r1',function(){
+/*Route::get('r1',function(){
     return redirect('r2');
 });
 Route::get('r2',function(){
@@ -23,4 +23,4 @@ Route::get('r2',function(){
 });
 Route::get('/',function(){
     return view('welcome');
-});
+});*/
