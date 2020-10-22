5df5b368c0e090701c05877c06764b17f50af07d
Pai Yen Chang <ken1100327@gmail.com>
2020-10-22 18:20:34 +0800

    練習4之1 將 Route 取名為 hello.index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/',function(){
     return'welcome';
 });
@@ -33,3 +34,8 @@ Route::get('hello/{name}',function($name){
 Route::get('hello/{name?}',function($name='Everybody'){
     return'Hello,'.$name;
 });
+*/
+
+Route::get('hello/{name?}', function ($name='Everybody') {
+    return 'Hello, '.$name;
+})->name('hello.index');
